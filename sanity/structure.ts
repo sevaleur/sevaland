1a727a798dde7faee14ef651261a5657888599b4
Sune Valeur <contact@sunevaleur.com>
2025-02-19 13:57:56 +0100

    created custom structure of studio path

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,5 +1,48 @@
 import type { StructureResolver } from "sanity/structure";
+import { LuHouse, LuImages, LuMenu, LuImage, LuUser } from "react-icons/lu";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
-  S.list().title("Content").items(S.documentTypeListItems());
+  S.list()
+    .title("SEVALAND")
+    .items([
+      S.listItem({
+        id: "home",
+        icon: LuHouse,
+      })
+        .title("Home")
+        .child(S.document().schemaType("home").documentId("home")),
+      S.listItem({
+        id: "projects",
+        icon: LuImages,
+      })
+        .title("Projects")
+        .child(
+          S.list()
+            .title("Projects")
+            .items([
+              S.listItem({
+                id: "menu",
+                icon: LuMenu,
+              })
+                .title("Menu")
+                .child(
+                  S.document().schemaType("project").documentId("project"),
+                ),
+              S.listItem({
+                id: "project",
+                icon: LuImage,
+              })
+                .title("Project")
+                .child(
+                  S.documentList().title("Project").filter("_type=='project'"),
+                ),
+            ]),
+        ),
+      S.listItem({
+        id: "about",
+        icon: LuUser,
+      })
+        .title("About")
+        .child(S.document().schemaType("about").documentId("about")),
+    ]);
