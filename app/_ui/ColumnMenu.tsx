506dd69f48b68481bb7bbad7f9be4f7ad2c41202
Sune Valeur <contact@sunevaleur.com>
2025-03-03 22:50:33 +0100

    preperation for GSAP implementation

--- app/_ui/ColumnMenu.tsx
+++ app/_ui/ColumnMenu.tsx
@@ -1,8 +1,13 @@
 import { getImageDimensions } from "@sanity/asset-utils";
 import { urlFor } from "@/sanity/lib/image";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 import Link from "next/link";
 import Image from "next/image";
+import gsap from "gsap";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const ColumnMenu = ({ data }: { data: Array<Record<PropertyKey, string>> }) => {
   return (
