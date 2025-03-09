28cf202ca0b6210072a1b9bb5eb7dfaa312d9155
Sune Valeur <contact@sunevaleur.com>
2025-03-09 14:57:05 +0100

    Progress made on home schema

--- sanity/schemaTypes/schemas/home.ts
+++ sanity/schemaTypes/schemas/home.ts
@@ -24,6 +24,7 @@ const home = defineType({
               name: "alt",
               type: "string",
               title: "Alternative text",
+              validation: (Rule) => Rule.required(),
             }),
           ],
         }),
@@ -38,6 +39,184 @@ const home = defineType({
       name: "description",
       type: "text",
       title: "Description",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "featured",
+      type: "object",
+      title: "Work Section",
+      fields: [
+        defineField({
+          name: "work",
+          type: "array",
+          title: "Work",
+          options: { layout: "list" },
+          validation: (Rule) => Rule.length(7).unique(),
+          of: [
+            defineField({
+              name: "image",
+              type: "image",
+              title: "Image",
+              fields: [
+                defineField({
+                  name: "alt",
+                  type: "string",
+                  title: "Alternative text",
+                  validation: (Rule) => Rule.required(),
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: "parallax",
+      type: "object",
+      title: "Text Parallax",
+      fields: [
+        defineField({
+          name: "images",
+          type: "array",
+          title: "Images",
+          options: { layout: "list" },
+          validation: (Rule) => Rule.length(3).unique(),
+          of: [
+            defineField({
+              name: "image",
+              type: "image",
+              title: "Image",
+              fields: [
+                defineField({
+                  name: "alt",
+                  type: "string",
+                  title: "Alternative text",
+                  validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                  name: "text",
+                  type: "string",
+                  title: "Text",
+                  validation: (Rule) => Rule.required(),
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: "shop",
+      type: "object",
+      title: "Shop section",
+      fields: [
+        defineField({
+          name: "title",
+          type: "string",
+          title: "Title",
+        }),
+        defineField({
+          name: "description",
+          type: "text",
+          title: "Description",
+        }),
+        defineField({
+          name: "link",
+          type: "string",
+          title: "Link text",
+        }),
+        defineField({
+          name: "prints",
+          type: "array",
+          title: "Prints",
+          options: { layout: "grid" },
+          validation: (Rule) => Rule.length(9).unique(),
+          of: [
+            defineField({
+              name: "image",
+              type: "image",
+              title: "Image",
+              fields: [
+                defineField({
+                  name: "alt",
+                  type: "string",
+                  title: "Alternative text",
+                  validation: (Rule) => Rule.required(),
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: "about",
+      type: "object",
+      title: "About section",
+      fields: [
+        defineField({
+          name: "title",
+          type: "string",
+          title: "Title",
+        }),
+        defineField({
+          name: "leftColumn",
+          type: "object",
+          title: "Left Column",
+          fields: [
+            defineField({
+              name: "image",
+              type: "image",
+              title: "Image",
+              fields: [
+                defineField({
+                  name: "alt",
+                  type: "string",
+                  title: "Alternative text",
+                  validation: (Rule) => Rule.required(),
+                }),
+              ],
+            }),
+            defineField({
+              name: "introduction",
+              type: "text",
+              title: "Introduction",
+            }),
+          ],
+        }),
+        defineField({
+          name: "description",
+          type: "text",
+          title: "Description",
+        }),
+        defineField({
+          name: "socials",
+          type: "array",
+          title: "Socials",
+          options: { layout: "list" },
+          of: [
+            defineField({
+              name: "icon",
+              type: "image",
+              title: "Icon",
+              fields: [
+                defineField({
+                  name: "alt",
+                  type: "string",
+                  title: "Alternative text",
+                  validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                  name: "href",
+                  type: "url",
+                  title: "Url",
+                  validation: (Rule) => Rule.required(),
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
     }),
   ],
 });
